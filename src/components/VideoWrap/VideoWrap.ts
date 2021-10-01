d79bb694f84ff420a074d03a84e58d4b9814b7d4
LenarFF <flfforwork@gmail.com>
2021-10-01 15:10:48 +0300

    feat: add styles for video player

--- src/components/VideoWrap/VideoWrap.ts
+++ src/components/VideoWrap/VideoWrap.ts
@@ -8,21 +8,28 @@ export class VideoWrap extends BaseComponent {
   fullScreen: BaseComponent;
   progressBar: HTMLInputElement;
   volumeBar: HTMLInputElement;
+  videoWrapper: BaseComponent;
 
   constructor(video: HTMLImageElement) {
-    super('div', ['video__video-wrap']);
-    this.element.append(video);
+    super('div', ['video__player']);
+
+    this.videoWrapper = new BaseComponent('div', ['video__player__wrap']);
+    this.videoWrapper.element.append(video);
+
+    this.element.append(this.videoWrapper.element);
     this.controls = new BaseComponent('div', ['video__controls']);
 
-    this.play = new BaseComponent('div', ['video__play','video__control']);
-    this.volume = new BaseComponent('div', ['video__volume','video__control']);
-    this.fullScreen = new BaseComponent('div', ['video__fullscreen','video__control']);
+    this.play = new BaseComponent('div', ['video__play', 'video__control']);
+    this.volume = new BaseComponent('div', ['video__volume', 'video__control']);
+    this.fullScreen = new BaseComponent('div', ['video__fullscreen', 'video__control']);
     this.progressBar = document.createElement('input');
     this.progressBar.type = 'range';
-    this.progressBar.classList.add('video__bar');
+    this.progressBar.classList.add('video__bar', 'video__progress-bar');
+    this.progressBar.addEventListener('input', (e) => this.changeBarColor(e));
     this.volumeBar = document.createElement('input');
     this.volumeBar.type = 'range';
-    this.volumeBar.classList.add('video__bar');
+    this.volumeBar.classList.add('video__bar', 'video__volume-bar');
+    this.volumeBar.addEventListener('input', (e) => this.changeBarColor(e));
 
     this.controls.element.append(
       this.play.element,
@@ -32,6 +39,11 @@ export class VideoWrap extends BaseComponent {
       this.fullScreen.element,
     );
 
-    this.element.append(this.controls.element)
+    this.element.append(this.controls.element);
   }
+
+  changeBarColor = (e: Event) => {
+    const input = e.target as HTMLInputElement;
+    input.style.background = `linear-gradient(to right, #710707 0%, #710707 ${input.value}%, #fff ${input.value}%, white 100%)`;
+  };
 }
--- src/content/videoContent.ts
+++ src/content/videoContent.ts
@@ -1,1 +1,7 @@
-export const videoContent = ['poster0.jpg', 'poster1.jpg', 'poster2.jpg', 'poster3.jpg', 'poster4.jpg'];
+export const videoContent = [
+  'poster0.jpg',
+  'poster1.jpg',
+  'poster2.jpg',
+  'poster3.jpg',
+  'poster4.jpg',
+];
