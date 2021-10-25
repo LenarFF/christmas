350769d44b8279212ef83ccc628e303e2e07b6eb
LenarFF <76207447+LenarFF@users.noreply.github.com>
2021-10-25 03:34:03 +0300

    add refresh function

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,10 @@ import { handleWeather } from './script/weather';
 
 addToLocalStorage();
 handleWeather();
+
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
