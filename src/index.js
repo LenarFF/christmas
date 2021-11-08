d7be34dfc2c1d2608ca0ba21171e5add021da100
LenarFF <flfforwork@gmail.com>
2021-11-08 09:17:16 +0300

    feat: add webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import bootstrap from 'bootstrap';
+
+const app = document.getElementById('root');
+
+const Button = document.createElement('button')
+
+console.log(bootstrap)
+const bsButton = new bootstrap.Button(Button);
+
+app.innerHTML = Button;
