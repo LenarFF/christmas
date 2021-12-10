f852e89c250e1890e9bbfbbfda2856f707f24acc
LenarFF <flfforwork@gmail.com>
2021-12-10 21:43:53 +0300

    feat: add layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,22 +5,23 @@ import ChristmasTreePage from '../../pages/ChristmasTreePage/ChristmasTreePage';
 import ErrorPage from '../../pages/ErrorPage/ErrorPage';
 import StartPage from '../../pages/StartPage/StartPage';
 import ToysPage from '../../pages/ToysPage/ToysPage';
-import Navigation from '../Navigation/Navigation';
+import Layout from '../Layout/Layout';
 
 function App(): JSX.Element {
   const location = useLocation();
 
   return (
     <>
-      <Navigation />
       <div className="container">
         <TransitionGroup>
-          <CSSTransition timeout={300} classNames="page" key={location.key} unmountOnExit>
+          <CSSTransition timeout={100} classNames="page" key={location.key} unmountOnExit>
             <Routes location={location}>
-              <Route path="/" element={<StartPage />} />
-              <Route path="/toys" element={<ToysPage />} />
-              <Route path="/tree" element={<ChristmasTreePage />} />
-              <Route path="*" element={<ErrorPage />} />
+              <Route path="/" element={<Layout/>}>
+                <Route index element={<StartPage />} />
+                <Route path="toys" element={<ToysPage />} />
+                <Route path="tree" element={<ChristmasTreePage />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Route>
             </Routes>
           </CSSTransition>
         </TransitionGroup>
